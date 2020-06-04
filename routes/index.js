ddb575becbcf56f50e79afe2b68c5e5b8a6819ba
ogwurujohnson <ogwurujohnson@gmail.com>
2020-06-04 03:06:04 +0100

    🏍update: establish route for authentication

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const campaigns = require('./campaigns');
+const auth = require('./auth');
 
 function routes (app) {
     app.use('/api/campaigns', campaigns);
+    app.use('/api/auth', auth);
     app.get('/', (req, res) => {
         res.status(200).json({
             api: "running"
