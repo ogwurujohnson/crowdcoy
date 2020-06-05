464f5bc2d8ea890166ee056b7ad66d0035dfe837
ogwurujohnson <ogwurujohnson@gmail.com>
2020-06-05 03:08:12 +0100

    👌Feat: implement registeration service

--- services/auth.js
+++ services/auth.js
@@ -10,3 +10,28 @@ function generateToken(user) {
         }
     )
 }
+
+const register = async ({username, password, email}) => {
+    const user = await User.findOne({
+        username
+    });
+
+    if (user) {
+        responses = { status: 403, message: 'user with username already exists' }
+        return responses;
+    }
+    password = await bcrypt.hash(password, 12);
+
+    const newUser = new User({
+        username,
+        password,
+        email,
+        createdAt: new Date().toISOString()
+    });
+
+    const res = await newUser.save();
+    const token = generateToken(res);
+    responses = { status: 201, data: {...res._doc, id: res.id}, token }
+    return responses;
+}
+
