d0679f702c286608d81d7b8add63511addbf1f0c
ogwurujohnson <ogwurujohnson@gmail.com>
2020-06-05 03:03:17 +0100

    🚀Feat: implement the user schema of camapaigns

--- models/Campaign.js
+++ models/Campaign.js
@@ -10,11 +10,16 @@ const campaignSchema = new Schema({
     received: String,
     deadline: String,
     isActive: Boolean,
+    username: String,
     createdAt: String,
     funders: [{
         address: String,
         amount: String
-    }]
+    }],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    }
 });
 
 module.exports = model('Campaign', campaignSchema);
